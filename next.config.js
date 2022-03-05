5d0cfa4c019ac090c33fddce18cb355a484a3a12
Gianluca Carucci <gianluca.carucci@gucci.com>
2022-03-05 15:35:37 +0100

    render legacy as redirect

--- next.config.js
+++ next.config.js
@@ -1,12 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // https://github.com/vercel/next.js/issues/21079
-  // Remove this workaround whenever the issue is fixed
-  images: {
-    loader: "imgix",
-    path: "",
+  async rewrites() {
+    return [
+      {
+        source: "/legacy",
+        destination: "/legacy/index.htm",
+      },
+    ];
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/legacy",
+        permanent: true,
+      },
+    ];
+  },
+  //https://github.com/vercel/next.js/issues/21079
+  //Remove this workaround whenever the issue is fixed
+  images: {
+    loader: "custom",
+  },
+  // images: {
+  //   loader: "imgix",
+  //   path: "",
+  // },
 };
 
 module.exports = nextConfig;
