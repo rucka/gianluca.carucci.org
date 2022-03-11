923af625337375928b0f3f63dd39b89f8d3fcb2b
Gianluca Carucci <gianluca.carucci@gucci.com>
2022-03-11 14:12:13 +0100

    add styled components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,41 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+html{
+  box-sizing: border-box;
+  background: #F5F4F0;
+  display:block;
+  height: 100%;
+  max-width: 640px;
+  margin:0 auto;
+  padding: 0;
+}
+
+body{
+  background-color:#fafafa;
+  min-height:100vh;
+  padding: 1rem;
+  margin-top:0;
+  font-family:Verdana;
+}
+`
+
+const theme = {
+  colors: {
+    primary: '#fafafa'
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
