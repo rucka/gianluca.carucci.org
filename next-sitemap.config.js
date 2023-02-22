667e2db552d97d58c9c9a8ac1ba5f5af3d3fb9c2
Gianluca Carucci <rucka@tiscalinet.it>
2023-02-22 19:33:40 +0100

    fix default siteUrl

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: process.env.SITE_URL || 'https://carucci.org',
+  siteUrl: process.env.SITE_URL || 'https://gianluca.carucci.org',
   generateRobotsTxt: true,
   sitemapSize: 1000,
   exclude: ['/l', '/q', '/conferma', '/sitemap.xml']
