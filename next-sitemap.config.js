036867bb9795ceaa1d7b356b4468b81fec8a95e6
Gianluca Carucci <rucka@tiscalinet.it>
2023-02-23 12:46:30 +0100

    update sitemap and robot.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,8 +2,17 @@
 const config = {
   siteUrl: process.env.SITE_URL || 'https://gianluca.carucci.org',
   generateRobotsTxt: true,
-  sitemapSize: 1000,
-  exclude: ['/l', '/q', '/conferma', '/sitemap.xml']
+  sitemapSize: 5000,
+  exclude: ['/en', '/l', '/q', '/l/*', '/q/*', '/conferma', '/sitemap.xml'],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/en', '/l', '/q', '/l/*', '/q/*', '/conferma']
+      }
+    ]
+  }
 }
 
 module.exports = config
